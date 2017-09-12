dfadd98d12cdf3c3c7e51bd8f9059cf9763b0433
Yuanyuan Li <yl2577@email.com>
2017-09-12 00:29:15 -0400

    add server

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const path = require('path');
+var app = express();
+
+app.use(express.static(__dirname + '/build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log(`Password generator listening on ${port}`);
+});
